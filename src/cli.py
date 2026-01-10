12cff08d27f2dffb27c7b5c9841ad17f2ca4e67d
MANISH-BEESA <manishbeesa05@gmail.com>
2026-01-10 14:53:03 +0530

    setup completed

--- src/cli.py
+++ src/cli.py
@@ -1,57 +1,5 @@
-import argparse
-from src import data
-
-
-def init(args):
-    data.init(args.path)
-
-
-def hash_objects(args):
-    data.hash_objects(args.file)
-
-
-def decompress(args):
-    data.decompress(args.hash)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(prog="ding")
-
-    # COMMANDS
-    commands = parser.add_subparsers(dest="command", required=True)
-
-    ## init
-    init_parser = commands.add_parser(
-        "init", help="initializes an empty ding repository"
-    )
-    init_parser.add_argument(
-        "path",
-        nargs="?",
-        default=".",
-        help="directory to initialize the ding repository in (default: current directory)",
-    )
-    init_parser.set_defaults(func=init)
-
-    ## hash
-    hash_parser = commands.add_parser("hash", help="hashes and stores the file")
-    hash_parser.add_argument("file", help="the file to be hashed")
-    hash_parser.set_defaults(func=hash_objects)
-
-    ## cat-file
-    hash_parser = commands.add_parser(
-        "cat-file", help="prints out the uncompressed data"
-    )
-    hash_parser.add_argument(
-        "hash", help="the hash (or start of hash) of file to decompress"
-    )
-    hash_parser.set_defaults(func=decompress)
-
-    return parser.parse_args()
-
-
-def main(): 
-    args = parse_args()
-    args.func(args)
+def main() : 
+    print("Ding Dong, who's there \nThe IRS \nOh Shit")
 
 
 if __name__ == "__main__":
